8eecedf1fa28c6795bfedb6f3fe4b1f67b480008
Zachary Davy <zacharyjdavy@gmail.com>
2014-01-22 10:54:12 -0600

    first commit

---
+++ spec/refactor_ttt/core_spec.clj
@@ -0,0 +1,7 @@
+(ns refactor-ttt.core-spec
+  (:require [speclj.core :refer :all]
+            [refactor-ttt.core :refer :all]))
+
+(describe "a test"
+  (it "FIXME, I fail."
+    (should= 1 1)))
---
+++ src/refactor_ttt/core.clj
@@ -0,0 +1,1 @@
+(ns refactor-ttt.core)
