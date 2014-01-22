75a843dcc1f6776449f15f8fa3444257bc98f02d
Zachary Davy <zacharyjdavy@gmail.com>
2014-01-22 11:50:47 -0600

    add board module
    Responsibilities include determining the marker values, creating a new
    board, making a move, returning the index of spaces, and displaying the
    board.

---
+++ src/refactor_ttt/board.clj
@@ -0,0 +1,26 @@
+(ns refactor-ttt.board)
+
+(def mark
+  {:blank "-"
+   :player "x"
+   :computer "o"})
+
+(def new-board
+  (vec (repeat 9 (:blank mark))))
+
+(defn move [board index turn]
+  (assoc board index (turn mark)))
+
+(defn available-spaces [board]
+  (keep-indexed (fn[index space] (if (= space (:blank mark)) index)) board))
+
+(defn render [board index]
+  (str " " (if (= (get board index) "-") index (get board index)) " "))
+
+(defn show [board]
+  (println (str
+(render board 0) "|" (render board 1) "|"  (render board 2)
+"\n-----------\n"
+(render board 3) "|" (render board 4) "|" (render board 5)
+"\n-----------\n"
+(render board 6) "|" (render board 7) "|" (render board 8))))
