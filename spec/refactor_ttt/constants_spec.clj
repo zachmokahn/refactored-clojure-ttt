59cbddbeddb5cba7bc258e1f305c744b9d9d6dd3
Zachary Davy <zacharyjdavy@gmail.com>
2014-01-22 22:23:53 -0600

    add category describer

--- spec/refactor_ttt/constants_spec.clj
+++ spec/refactor_ttt/constants_spec.clj
@@ -2,13 +2,14 @@
   (:require [speclj.core :refer :all]
             [refactor-ttt.constants :refer :all]))
 
-(describe "markers"
-  (it "blank is '-'"
-      (should= "-"
-               (:blank mark)))
-  (it "player is 'x'"
-      (should= "x"
-               (:player mark)))
-  (it "computer is 'o'"
-      (should= "o"
-               (:computer mark))))
+(describe "Constants Spec"
+  (describe "markers"
+    (it "blank is '-'"
+        (should= "-"
+                 (:blank mark)))
+    (it "player is 'x'"
+        (should= "x"
+                 (:player mark)))
+    (it "computer is 'o'"
+        (should= "o"
+                 (:computer mark)))))
